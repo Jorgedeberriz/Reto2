72aff5f7006f3e10d810e5e8b20715d07927a019
Miguel <michigelo@hotmail.com>
2023-10-23 12:07:37 +0200

    refactorizacion

--- src/test/java/es/netmind/mypersonalbankapi/test/Test.java
+++ src/test/java/es/netmind/mypersonalbankapi/test/Test.java
@@ -1,9 +1,9 @@
-package es.netmind.mypersonalbankapi.controladores;
+package es.netmind.mypersonalbankapi.test;
 
+import es.netmind.mypersonalbankapi.controladores.ClientesController;
 import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
 import es.netmind.mypersonalbankapi.modelos.clientes.Personal;
 import es.netmind.mypersonalbankapi.persistencia.ClientesInMemoryRepo;
-import org.junit.jupiter.api.Test;
 import es.netmind.mypersonalbankapi.exceptions.*;
 
 import java.time.LocalDate;
@@ -12,8 +12,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class ClientesControllerTest {
-    @Test
+class Test {
+    @org.junit.jupiter.api.Test
     void altaCliente_Ok() throws Exception {
         Cliente cli = new Personal(4, "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
         String[] args = {"personal", "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", String.valueOf(LocalDate.now()), "87654321A"};
@@ -21,7 +21,7 @@ class ClientesControllerTest {
         assertThat(ClientesInMemoryRepo.getInstance().getClientById(4),samePropertyValuesAs(cli));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void dadoCliente_cuandoEmailIncorrecto_entoncesClienteException() throws Exception {
         Cliente cli = new Personal(4, "Nuevo cliente", "ncdxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
         assertThrows(ClienteException.class, () -> {
