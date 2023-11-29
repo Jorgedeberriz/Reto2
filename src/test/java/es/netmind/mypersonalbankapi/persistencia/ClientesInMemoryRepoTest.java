25a382bbfbac54645d32a5ebde29b10abc48b238
Miguel <michigelo@hotmail.com>
2023-11-29 12:28:34 +0100

    update

--- src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesInMemoryRepoTest.java
+++ src/test/java/es/netmind/mypersonalbankapi/persistencia/ClientesInMemoryRepoTest.java
@@ -24,70 +24,68 @@ import static org.hamcrest.Matchers.*;
 @ContextConfiguration(classes = {SpringConfig.class})
 @ActiveProfiles("dev")
 class ClientesInMemoryRepoTest {
-    //@Autowired
-    //private ApplicationContext context;
     @Autowired
     private IClientesRepo repo;
- /*@Test
+ @Test
     void cuandoClienteCorrecto_entoncesAltaOk() throws Exception {
-        Cliente cli = new Personal(4, "Nombre nuevo", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
-        ClientesInMemoryRepo.getInstance().addClient(cli);
-        assertThat(ClientesInMemoryRepo.getInstance().getClientById(4), samePropertyValuesAs(cli));
+        Cliente cli = new Personal(5, "Nombre nuevo", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
+        repo.addClient(cli);
+        assertThat(repo.getClientById(5), samePropertyValuesAs(cli));
     }
 
     @Test
     void dadoCliente_cuandoFechaIncorrecta_entoncesClienteException() throws Exception {
         String fechaString = "2023-10-22";
         LocalDate fechaLocalDate = LocalDate.parse(fechaString);
-        Cliente cli = new Personal(4, "Nuevo cliente", "ncdxc.com", "Calle Nueva 1", fechaLocalDate, true, false, "87654321A");
+        Cliente cli = new Personal(5, "Nuevo cliente", "ncdxc.com", "Calle Nueva 1", fechaLocalDate, true, false, "87654321A");
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
 
 
     @Test
     void dadoCliente_cuandoNombreIncorrecto_entoncesClienteException() throws Exception {
-        Cliente cli = new Personal(4, "Nom", "ncdxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
+        Cliente cli = new Personal(5, "Nom", "ncdxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
 
 
     @Test
     void dadoCliente_cuandoEmailIncorrecto_entoncesClienteException() throws Exception {
-        Cliente cli = new Personal(4, "Nuevo cliente", "ncdxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
+        Cliente cli = new Personal(5, "Nuevo cliente", "ncdxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321A");
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
 
     @Test
     void dadoCliente_cuandoDireccionIncorrecta_entoncesClienteException() throws Exception {
-        Cliente cli = new Personal(4, "Nuevo cliente", "nc@dxc.com", "C/", LocalDate.now(), true, false, "87654321A");
+        Cliente cli = new Personal(5, "Nuevo cliente", "nc@dxc.com", "C/", LocalDate.now(), true, false, "87654321A");
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
 
     @Test
     void dadoClientePersonal_cuandoNifIncorrecto_entoncesClienteException() throws Exception {
-        Cliente cli = new Personal(4, "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321");
+        Cliente cli = new Personal(5, "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "87654321");
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
 
     @Test
     void dadoClienteEmpresa_cuandoCifIncorrecto_entoncesClienteException() throws Exception {
         String[] unidadesNegocio = {"Contabilidad"};
-        Cliente cli = new Empresa(4, "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "A8654321", unidadesNegocio);
+        Cliente cli = new Empresa(5, "Nuevo cliente", "nc@dxc.com", "Calle Nueva 1", LocalDate.now(), true, false, "A8654321", unidadesNegocio);
         assertThrows(ClienteException.class, () -> {
-            ClientesInMemoryRepo.getInstance().addClient(cli);
+            repo.addClient(cli);
         });
     }
-*/
+
     @Test
     void dadoIdClientePersonal_cuandoRecuperarPorId_entoncesOk() throws Exception {
         Cliente ncliente = repo.getClientById(4);
