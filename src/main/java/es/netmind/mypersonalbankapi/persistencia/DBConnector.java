1a98a6e1b97b33304705e5a8f489423874edb7f9
Miguel <michigelo@hotmail.com>
2023-11-29 12:32:17 +0100

    update

--- src/main/java/es/netmind/mypersonalbankapi/persistencia/DBConnector.java
+++ src/main/java/es/netmind/mypersonalbankapi/persistencia/DBConnector.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-        PropertyValues props = new PropertyValues();
+    PropertyValues props = new PropertyValues();
 
     public void connect() throws SQLException, IOException {
         String db_url = props.getPropValues().getProperty("db_url");
