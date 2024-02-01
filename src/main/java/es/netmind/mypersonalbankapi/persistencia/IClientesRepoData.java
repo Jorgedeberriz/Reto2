0274499f7a647e5d1c9d69bcdd8654b6eab970c3
Miguel <michigelo@hotmail.com>
2024-02-01 11:05:14 +0100

    update

--- src/main/java/es/netmind/mypersonalbankapi/persistencia/IClientesRepoData.java
+++ src/main/java/es/netmind/mypersonalbankapi/persistencia/IClientesRepoData.java
@@ -4,5 +4,4 @@ import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IClientesRepoData extends JpaRepository<Cliente, Integer> {
-     public Cliente findClienteById(Integer id);
 }
