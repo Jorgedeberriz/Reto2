b99f7dd0dfc5ab3f32d86c82d19ce1662e14c8a2
Miguel <michigelo@hotmail.com>
2024-02-01 10:45:01 +0100

    update

--- src/main/java/es/netmind/mypersonalbankapi/config/SpringConfig.java
+++ src/main/java/es/netmind/mypersonalbankapi/config/SpringConfig.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @Import({PropertiesConfig.class,ReposConfig.class})
 @ComponentScan(basePackages = {"es.netmind.mypersonalbankapi.controladores","es.netmind.mypersonalbankapi.persistencia"})
 @EntityScan("es.netmind.mypersonalbankapi.modelos")
+@EnableJpaRepositories(basePackages = "es.netmind.mypersonalbankapi.persistencia")
 public class SpringConfig {
 }
--- src/main/java/es/netmind/mypersonalbankapi/persistencia/IClientesRepoData.java
+++ src/main/java/es/netmind/mypersonalbankapi/persistencia/IClientesRepoData.java
@@ -1,2 +1,8 @@
-package es.netmind.mypersonalbankapi.persistencia;public interface IClientesRepoData {
+package es.netmind.mypersonalbankapi.persistencia;
+
+import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClientesRepoData extends JpaRepository<Cliente, Integer> {
+     public Cliente findClienteById(Integer id);
 }
