4c05108ae3e69ddb11c4435c0c9f00a26f2de68c
purificaciondr <purificacion.dominguez@dxc.com>
2024-02-01 13:35:33 +0100

    prestamo controller

--- src/main/java/es/netmind/mypersonalbankapi/controladores/PrestamosController.java
+++ src/main/java/es/netmind/mypersonalbankapi/controladores/PrestamosController.java
@@ -3,61 +3,63 @@ package es.netmind.mypersonalbankapi.controladores;
 import es.netmind.mypersonalbankapi.exceptions.PrestamoException;
 import es.netmind.mypersonalbankapi.modelos.clientes.Cliente;
 import es.netmind.mypersonalbankapi.modelos.prestamos.Prestamo;
-import es.netmind.mypersonalbankapi.persistencia.ClientesInMemoryRepo;
-import es.netmind.mypersonalbankapi.persistencia.IClientesRepo;
-import es.netmind.mypersonalbankapi.persistencia.IPrestamosRepo;
-import es.netmind.mypersonalbankapi.persistencia.PrestamosInMemoryRepo;
+import es.netmind.mypersonalbankapi.persistencia.IClientesRepoData;
+import es.netmind.mypersonalbankapi.persistencia.IPrestamosRepoData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
+@Component
 public class PrestamosController {
-    private static IPrestamosRepo prestamosRepo = PrestamosInMemoryRepo.getInstance();
-    private static IClientesRepo clientesRepo = ClientesInMemoryRepo.getInstance();
+    @Autowired
+    private IPrestamosRepoData prestamosRepo;
+    @Autowired
+    private IClientesRepoData clientesRepo;
 
-    public static void mostrarLista(Integer uid) {
+    public void mostrarLista(Integer uid) {
         System.out.println("\nLista de prestamos del cliente: " + uid);
         System.out.println("───────────────────────────────────");
 
         try {
-            List<Prestamo> prestamos = prestamosRepo.getLoansByClient(uid);
+            List<Prestamo> prestamos = prestamosRepo.findByMyCliente_Id(uid);
             if (prestamos != null && prestamos.size() > 0) System.out.println(prestamos);
             else System.out.println("El cliente no tiene prestamos!");
-        } catch (PrestamoException e) {
-            System.out.println("Cliente NO encontrado 😞! \nCode: " + e.getCode());
         } catch (Exception e) {
             System.out.println("Oops ha habido un problema, inténtelo más tarde 😞!");
         }
     }
 
-    public static void mostrarDetalle(Integer uid, Integer lid) {
+    public void mostrarDetalle(Integer uid, Integer lid) {
         System.out.println("\nDetalle de prestamo: " + lid + ", del cliente: " + uid);
         System.out.println("───────────────────────────────────");
 
         try {
-            Prestamo prestamo = prestamosRepo.getLoansByClientAndId(uid, lid);
+            Optional<Prestamo> opPrestamo = prestamosRepo.findById(lid);
+            Prestamo prestamo = opPrestamo.get();
             System.out.println(prestamo);
-        } catch (PrestamoException e) {
-            System.out.println("Prestamo NO encontrado para el cliente 😞! \nCode: " + e.getCode());
         } catch (Exception e) {
             System.out.println("Oops ha habido un problema, inténtelo más tarde 😞!");
         }
     }
 
-    public static void eliminar(Integer uid, Integer lid) {
+    public void eliminar(Integer uid, Integer lid) {
         System.out.println("\nBorrando prestamo: " + lid + ", para cliente: " + uid);
         System.out.println("───────────────────────────────────");
 
         try {
-            Prestamo pr = prestamosRepo.getLoansByClientAndId(uid, lid);
-            boolean borrado = prestamosRepo.deleteLoan(pr);
-            if (borrado) {
-                Cliente cl = clientesRepo.getClientById(uid);
+            Optional<Prestamo> opPrestamo = prestamosRepo.findById(lid);
+            Prestamo pr = opPrestamo.get();
+
+            prestamosRepo.delete(pr);
+            //if (borrado) {
+                Optional<Cliente> opCl = clientesRepo.findById(uid);
+                Cliente cl = opCl.get();
                 cl.delisgarPrestamo(pr);
                 System.out.println("Préstamo borrada 🙂!!");
                 mostrarLista(uid);
-            } else System.out.println("Préstamo NO borrado 😞!! Consulte con su oficina.");
-        } catch (PrestamoException e) {
-            System.out.println("Préstamo NO encontrado 😞! \nCode: " + e.getCode());
+            //} else System.out.println("Préstamo NO borrado 😞!! Consulte con su oficina.");
         } catch (Exception e) {
             System.out.println("Oops ha habido un problema, inténtelo más tarde 😞!");
         }
---
+++ src/test/java/es/netmind/mypersonalbankapi/controladores/PrestamosControllerTest.java
@@ -0,0 +1,54 @@
+package es.netmind.mypersonalbankapi.controladores;
+
+import es.netmind.mypersonalbankapi.config.SpringConfig;
+import es.netmind.mypersonalbankapi.persistencia.IPrestamosRepoData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringConfig.class})
+@ActiveProfiles("dev")
+@EnableAutoConfiguration
+class PrestamosControllerTest {
+private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    @Autowired
+    private IPrestamosRepoData repo;
+
+    @Autowired
+    private PrestamosController precon;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void dadoUnClienteMostrarLista() {
+        precon.mostrarLista(1);
+        assertThat(outContent.toString(), containsString("1"));
+    }
+
+}
