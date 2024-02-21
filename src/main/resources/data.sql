c3b22a8b593b840749be285ab7578b2183ef9e46
Miguel <michigelo@hotmail.com>
2024-02-21 13:30:26 +0100

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,6 @@ INSERT INTO `cliente` (`dtype`, `id`, `activo`, `alta`, `direccion`, `email`, `m
 --('Ahorro', 3, 0.2, '2023-11-07', 1.1, 300, 2),
 --('Ahorro', 4, 0.2, '2023-11-07', 1.1, 300, 1);
 
---INSERT INTO `prestamo` (`id`, `anios`, `fecha_concesion`, `interes`, `interes_mora`, `liquidado`, `mensualidad`, `monto`, `moroso`, `saldo`, `cliente_id`) VALUES
---(1, 2, '2023-11-07', 4, 2, '0', NULL, 1000, '0', 1000, 1),
---(2, 25, '2023-11-07', 4, 2, '0', NULL, 100000, '0', 100000, 3);
+INSERT INTO `prestamo` (`id`, `anios`, `fecha_concesion`, `interes`, `interes_mora`, `liquidado`, `mensualidad`, `monto`, `moroso`, `saldo`, `cliente_id`) VALUES
+(1, 2, '2023-11-07', 4, 2, '0', NULL, 1000, '0', 1000, 1),
+(2, 25, '2023-11-07', 4, 2, '0', NULL, 100000, '0', 100000, 3);
